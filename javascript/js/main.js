27b0a0028af6eca1fa29f5283b9cec376087647c
David <okekedavid1333@gmail.com>
2024-06-27 17:52:23 +0100

    js class

---
+++ javascript/js/main.js
@@ -0,0 +1,16 @@
+let a = 10
+let b = 9
+let c = 0
+let d = "Sammy"
+let e = "Tess"
+// a+=10 // a=a+10
+console.log(a)
+console.log(a<=b)
+if(c){
+    console.log(c)
+}
+
+console.log(d + " is tall")
+console.log(`${d} is taller than ${e}`)
+
+d && console.log('this is cool')
